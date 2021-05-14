ec79f2fee3ce8908174dfb3c2f1674d3da269216
Kiryl Nazarau <kiryl.nazarau@plus4u.net>
2021-05-14 10:35:38 +0200

    commit

---
+++ uun_bcaa21sft04_maing01-hi/test/bricks/read-ignredients.test.js
@@ -0,0 +1,11 @@
+import UU5 from "uu5g04";
+import UunBcaa21sft04 from "uun_bcaa21sft04_maing01-hi";
+
+const { shallow } = UU5.Test.Tools;
+
+describe(`UunBcaa21sft04.Bricks.ReadIgnredients`, () => {
+  it(`default props`, () => {
+    const wrapper = shallow(<UunBcaa21sft04.Bricks.ReadIgnredients />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
