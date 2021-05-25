a48df84475cc107f2c80309281eb745acd888e24
Matej Pilik <matej.pilik@plus4u.net>
2021-05-25 13:34:59 +0200

    Save v0.1

--- uun_bcaa21sft04_maing01-hi/src/bricks/save-recipe.js
+++ uun_bcaa21sft04_maing01-hi/src/bricks/save-recipe.js
@@ -20,7 +20,40 @@ export const SaveRecipe = createComponent({
   //@@viewOn:defaultProps
   defaultProps: {},
   //@@viewOff:defaultProps
-
+  const path = require("path"),
+//@fixme  const uuCookBook = require("??????");
+  let recipeSaved = new cookBookLibrary(path.join(__dirname, "..", "..", "storage", "recipes.json"))
+  
+  async function UpdateLib(req, res) {
+      let {id, name, ingredientsList} = req.body;
+      if (
+          (id && typeof id === "string" && id.length < 25) &&
+          (name && typeof name === "string" && name.length < 200) &&
+          (ingredientsList && ingredientsList.length > 0 && ingredientsList.length < 10)
+      ) {
+          const recipe = {id, name, ingredientsList, howTo};
+          try {
+              let result = await cookBook.updateRecipe(recipe);
+              res.status(200).json(result);
+          } catch (e) {
+              if (e.code === "FAILED_TO_GET_RECIPE") {
+                  res.status(400).json({error: e})
+              } else if (e.code === "FAILED_TO_UPDATE_RECIPE") {
+                  res.status(500).json({error: e})
+              } else {
+                  res.status(500).json({error: e})
+              }
+          }
+      } else {
+          res.status(400).json({
+              "error": "Invalid dtoIn"
+          })
+      }
+  }
+  
+  module.exports = SaveRecipe;
+ 
+ 
   render(props) {
     //@@viewOn:private
     //@@viewOff:private
@@ -28,7 +61,7 @@ export const SaveRecipe = createComponent({
     //@@viewOn:interface
     //@@viewOff:interface
 
-    //@@viewOn:render
+/*    //@@viewOn:render
     const className = Config.Css.css``;
     const attrs = UU5.Common.VisualComponent.getAttrs(props, className);
     const currentNestingLevel = UU5.Utils.NestingLevel.getNestingLevel(props, STATICS);
@@ -44,3 +77,4 @@ export const SaveRecipe = createComponent({
 });
 
 export default SaveRecipe;
+/*
