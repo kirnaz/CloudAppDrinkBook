c5e298293509f5951b1d3913dddd6587b1eae96d
Kiryl Nazarau <kiryl.nazarau@plus4u.net>
2021-06-02 15:17:48 +0200

    reed recipes 2.commit

--- uun_bcaa21sft04_maing01-hi/src/bricks/read-recipes.js
+++ uun_bcaa21sft04_maing01-hi/src/bricks/read-recipes.js
@@ -1,6 +1,6 @@
 //@@viewOn:imports
 import UU5 from "uu5g04";
-import { createComponent } from "uu5g04-hooks";
+import { createComponent, useState, useEffect } from "uu5g04-hooks";
 import Config from "./config/config";
 //@@viewOff:imports
 
@@ -9,7 +9,6 @@ const STATICS = {
   displayName: Config.TAG + "ReadRecipes",
   //@@viewOff:statics
 };
-
 export const ReadRecipes = createComponent({
   ...STATICS,
 
@@ -21,15 +20,36 @@ export const ReadRecipes = createComponent({
   defaultProps: {},
   //@@viewOff:defaultProps
 
-  render(props) {
+  render({ children }) {
     //@@viewOn:private
     //@@viewOff:private
 
     //@@viewOn:interface
     //@@viewOff:interface
+    const [recipes, setRecipes] = useState({ loading: false });
+    useEffect(() => {
+      const fetchData = async () => {
+        if (!recipes.loading && !recipes.data) {
+          setRecipes({ loading: true })
 
+          const response = await fetch(`http://localhost:3001/api/recipes/list`)
+          const data = await response.json()
+
+          if (data.error) {
+            alert(data.error)
+          } else {
+
+            setRecipes({ loading: false, data: data.value });
+          }
+
+
+        }
+      }
+      fetchData();
+    })
     //@@viewOn:render
-    const className = Config.Css.css``;
+    return children({ recipes: recipes.data })
+    /*const className = Config.Css.css``;
     const attrs = UU5.Common.VisualComponent.getAttrs(props, className);
     const currentNestingLevel = UU5.Utils.NestingLevel.getNestingLevel(props, STATICS);
 
@@ -38,9 +58,10 @@ export const ReadRecipes = createComponent({
         <div>Component {STATICS.displayName}</div>
         {UU5.Utils.Content.getChildren(props.children, props, STATICS)}
       </div>
-    ) : null;
+    ) : null;*/
     //@@viewOff:render
   },
 });
 
 export default ReadRecipes;
+
