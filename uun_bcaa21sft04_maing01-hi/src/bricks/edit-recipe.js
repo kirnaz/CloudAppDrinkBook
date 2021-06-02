108a8cd07e13b9a78873edd4743df8bde11c0241
Kiryl Nazarau <kiryl.nazarau@plus4u.net>
2021-06-02 20:52:59 +0200

    done

--- uun_bcaa21sft04_maing01-hi/src/bricks/edit-recipe.js
+++ uun_bcaa21sft04_maing01-hi/src/bricks/edit-recipe.js
@@ -1,6 +1,6 @@
 //@@viewOn:imports
 import UU5 from "uu5g04";
-import { createVisualComponent } from "uu5g04-hooks";
+import { createVisualComponent,useState } from "uu5g04-hooks";
 import Config from "./config/config";
 //@@viewOff:imports
 
@@ -20,6 +20,7 @@ export const EditRecipe = createVisualComponent({
 
   //@@viewOn:defaultProps
   defaultProps: {
+    id:null,
     recipe: null,
     ingredients: null,
     categoryList: null,
@@ -30,8 +31,9 @@ export const EditRecipe = createVisualComponent({
   //@@viewOff:defaultProps
 
   render({ recipe, ingredients, categoryList, recipeEdit, recipeDelete, ingredientSave }) {
-    //@@viewOn:private
-    //@@viewOff:private
+    //@@viewOn:hooks
+    const [recipee, setRecipee] = useState(recipe);
+    //@@viewOff:hooks
 
     //@@viewOn:interface
     //@@viewOff:interface
@@ -44,53 +46,94 @@ export const EditRecipe = createVisualComponent({
     function editRecipe(opt) {
       //TODO projde všechny ingredience a nové vytvoří
 
+
       recipeEdit(opt.values);
     }
 
-    if (!recipe || !ingredients || !categoryList) return <h1>Error</h1>
+    function _addLine() {
+        const newArray = steps.concat("");
+        setSteps(newArray);  
+    }
+
+    function _removeLine(i) {
+      if (steps.length > 1) {
+        const newArray = steps.filter((_, index) => index !== i);
+        setSteps(newArray);
+        setResult(JSON.stringify(steps));
+      }
+    }
+    function _onChangeLine(value, index) {
+      setResult(JSON.stringify(steps));
+    }
+
+    const [steps, setSteps] = useState(recipe.steps);
+    const [result, setResult] = useState(recipe.steps);
+
+    function _addLine(){
+      console.log("line+");
+      recipe.steps=[...recipe.steps,""];
+      setRecipee(recipe)
+      //render( recipe, ingredients, categoryList, recipeEdit, recipeDelete, ingredientSave );
+    }
 
     console.log(recipe);
     console.log(ingredients);
     console.log(categoryList);
 
+
+
+    if (!recipe || !ingredients || !categoryList) return null
+
     return (
+
       <div>
-        <UU5.Bricks.Header>
+        {/*<UU5.Bricks.Header>
           My recipes
-        </UU5.Bricks.Header>
+        </UU5.Bricks.Header>*/}
         <UU5.Bricks.Button onClick={recipeDelete}>Delete</UU5.Bricks.Button>
         <UU5.Bricks.Button onClick={recipeEdit}>Save</UU5.Bricks.Button>
 
         <UU5.Forms.Form>
-          <UU5.Forms.Text name="recipeName" value={recipe.recipeName} label="Name" />
-          <UU5.Forms.Number name="timeForPreparation" value={recipe.timeForPreparation} min={1} label="Time fo preparation" />
-          <UU5.Forms.Number name="numberOfPortions" value={recipe.numberOfPortions} min={1} label="Number of portions" />
-          <UU5.Forms.SwitchSelector name="category" items={categoryList.map(value => ({ value: value.name }))} label="Category" /*value={} onChange={({ value }) => this.setState({ size: value })}*/ />
+
+          <UU5.Forms.Text name="recipeName" value={recipe.recipeName} label="Name" required/>
+          <UU5.Forms.Number name="timeForPreparation" value={recipe.timeForPreparation} min={1} label="Time fo preparation" required/>
+          <UU5.Forms.Number name="numberOfPortions" value={recipe.numberOfPortions} min={1} label="Number of portions" required/>
+          <UU5.Forms.SwitchSelector name="category" items={categoryList.map(value => ({ value: value.name }))} label="Category" value={categoryList.find((cat)=>cat.id===recipe.category).name} /*onChange={({ value }) => this.setState({ size: value })}*/ required/>
           <UU5.Bricks.Table name="listOfIngredients" label="Ingredients">
             <UU5.Bricks.Table.TBody>
-              {recipe.listOfIngredients.map((ingredient) => {
-                <UU5.Bricks.container>
-                  123
-              <label>ingredient</label>
-                  <input type='text' name="ingredient" value={ingredient.id} list='listid' />
+              {recipe.listOfIngredients.map((ingredient) => (
+                <UU5.Bricks.Table.Tr>
+                <UU5.Bricks.Table.Td >
+                  {/*<label>ingredient</label>
+                  <input type='text' name="ingredient" value={ingredient.ingredientName} list='listid' />
                   <datalist id='listid'>
-                    {ingredients.map((ingredient) => { <option label={ingredient.ingredientName} value={ingredient.ingredientID} /> })}
-                  </datalist>
-                  <UU5.Forms.Number value={ingredient.ingredientAmount} label="amount" />
+                    {ingredients.map((ingredient) => { <option label={ingredient.ingredientName} value={ingredients.find((ing)=>ingredient.ingredientName===ing.ingredientName).ingredientID} /> })}
+                  </datalist>*/}
+                    <UU5.Forms.Select label="ingredient" value={ingredient.ingredientName} required>
+                      {ingredients.map((ingredient) => {return <UU5.Forms.Select.Option value={ingredient.ingredientName}/>})}
+                    </UU5.Forms.Select>
+                  <UU5.Forms.Number name="ingredientAmount" value={ingredient.ingredientAmount} min={1} label={`amount (${ingredient.ingredientMeasure})`} required/>
                   <UU5.Bricks.Button>remove</UU5.Bricks.Button>
-                </UU5.Bricks.container>
-              })}
+                  </UU5.Bricks.Table.Td></UU5.Bricks.Table.Tr>
+              ))}
             </UU5.Bricks.Table.TBody>
+            <UU5.Bricks.Table.Tr><UU5.Bricks.Table.Td>
             <UU5.Bricks.Button>add</UU5.Bricks.Button>
+            <UU5.Bricks.Button>add new</UU5.Bricks.Button></UU5.Bricks.Table.Td></UU5.Bricks.Table.Tr>
           </UU5.Bricks.Table>
           <UU5.Bricks.Table>
             <UU5.Bricks.Table.TBody>
               {recipe.steps.map((step, index) => (
                 <UU5.Bricks.Table.Tr key={index}>
-                  <UU5.Bricks.Table.Td ><UU5.Forms.Text value={step} /></UU5.Bricks.Table.Td>
+                  <UU5.Bricks.Table.Td><UU5.Forms.Text value={step} name={"step" + index} onBlur={(value) => _onChangeLine(value.value, index)}required/></UU5.Bricks.Table.Td>
+                  <UU5.Bricks.Button onClick={_removeLine}>Remove</UU5.Bricks.Button>
                 </UU5.Bricks.Table.Tr>
               ))}
-            </UU5.Bricks.Table.TBody>
+              <UU5.Forms.Text name="test" value={result} hidden={true} />
+              </UU5.Bricks.Table.TBody>
+              <UU5.Bricks.Table.TFoot>
+            <UU5.Bricks.Table.Tr><UU5.Bricks.Table.Td>
+            <UU5.Bricks.Button onClick={_addLine}>add</UU5.Bricks.Button></UU5.Bricks.Table.Td></UU5.Bricks.Table.Tr></UU5.Bricks.Table.TFoot>
 
           </UU5.Bricks.Table>
 
